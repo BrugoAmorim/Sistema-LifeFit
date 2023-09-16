c98428939e8f6219a93bc07eb57ae30dd0699315
BrugoAmorim <brunogomes.concomercial@gmail.com>
2023-09-16 19:00:48 -0300

    commit function createaccount javascript

--- System/Interface/public/javascript/criarconta.js
+++ System/Interface/public/javascript/criarconta.js
@@ -12,3 +12,43 @@ const closeCreateAccount = () => {
         inputs[i].value = '';
     }
 }
+
+const usernameinp = document.getElementById("username-inp");
+const emailinp = document.getElementById("useremail-inp");
+const passwordinp = document.getElementById("password-inp");
+const confirmpasswordinp = document.getElementById("confirmpassword-inp");
+
+const CreateAccount = async () => {
+
+    const url = "http://localhost:5090/access/createaccount";
+    const reqCreateAcc = {
+        username: usernameinp.value,
+        emailuser: emailinp.value,
+        newpassword: passwordinp.value,
+        confirmpassword: confirmpasswordinp.value
+    }
+
+    const api = await fetch(url, {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(reqCreateAcc)
+    });
+
+    const res = api.json();
+    res.then((data) => {
+
+        if(data.error == 400)
+            swal(data.message, "", "error");
+        else{
+            usernameinp.value = "";
+            emailinp.value = "";
+            passwordinp.value = "";
+            confirmpasswordinp.value = "";
+        
+            swal("Account created successufly", "", "success");
+        }
+    })
+}
